5b4dad2c377b1daced1f059183d0b60daa4faf8b
José Roberto <joserobertomiranda.contato@gmail.com>
2025-05-25 21:37:13 -0300

    Updated Home Page
    added initial info, buttons and logo image.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui'), require('tailwindcss-animated')],
+  plugins: [require('daisyui'), require('tailwindcss-animated'), require("@eioluseyi/tailwind-fade")],
 }
