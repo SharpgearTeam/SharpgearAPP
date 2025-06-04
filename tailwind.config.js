b6d5146cec9f524b59d1a7dcfa645a4e6431280f
NightlyOneV <joserobertomiranda.contato@gmail.com>
2025-06-04 09:41:34 -0300

    New font + resizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [require('daisyui'), require('tailwindcss-animated'), require("@eioluseyi/tailwind-fade")],
 }
