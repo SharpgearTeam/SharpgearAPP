8d6313a38b818d1e2db10933abf226ecfd2fbe31
thaue kleber de oliveira <thauekleber@gmail.com>
2025-06-17 11:39:04 -0300

    login system init

--- src/php/conexao.php
+++ src/php/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
 $host = "localhost";
-$dbmame = "";
+$dbname = "sharpgear_users";
 $usuario  = "root";
 $senha = "";
 
 try{
-    $pdo = new PDO("mysql:host=$host,dbname=$dbname ;charset=utf8", $usuario, $senha);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("erro de conexão:". $e->getMessage());
